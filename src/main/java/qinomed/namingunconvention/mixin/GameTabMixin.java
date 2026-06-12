f5ae8ded5633f02f8f33d9557b19181b6aa9cd53
QinomeD <cpueater@interia.pl>
2026-06-12 19:52:28 +0200

    I achieved working

--- src/main/java/qinomed/namingunconvention/mixin/GameTabMixin.java
+++ src/main/java/qinomed/namingunconvention/mixin/GameTabMixin.java
@@ -1,11 +1,13 @@
 package qinomed.namingunconvention.mixin;
 
+import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.client.gui.components.tabs.GridLayoutTab;
 import net.minecraft.client.gui.layouts.GridLayout;
 import net.minecraft.client.gui.screens.worldselection.CreateWorldScreen;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,40 +19,35 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import qinomed.namingunconvention.NamingUnconvention;
 
+import java.util.function.Consumer;
+
 @Mixin(CreateWorldScreen.GameTab.class)
-public class GameTabMixin {
+public class GameTabMixin extends GridLayoutTab{
     @Unique
     private ResourceLocation BTN_REROLL = new ResourceLocation(NamingUnconvention.MODID, "textures/reroll.png");
 
     @Shadow @Final
     public EditBox nameEdit;
-    @Shadow
-    @Final
-    CreateWorldScreen this$0;
+
     @Unique
     private Button rerollButton;
+
+    public GameTabMixin(Component pTitle) {
+        super(pTitle);
+    }
 
     @Inject(method = "<init>", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILHARD)
     private void randomName(CreateWorldScreen screen, CallbackInfo ci, GridLayout.RowHelper gridlayout$rowhelper1) {
         this.nameEdit.setValue(NamingUnconvention.RANDOM_NAME_GENERATOR.generateRandomName());
 
-        GridLayout.RowHelper helperGrid = (new GridLayout()).rowSpacing(2).createRowHelper(2);
-
-        if (((GridLayoutTab) ((Object) this)).layout.children.get(0) instanceof GridLayout worldNameGrid) {
-            helperGrid.addChild(worldNameGrid.children.get(1), helperGrid.newCellSettings().padding(1));
-            helperGrid.addChild(new ImageButton(0, 0, 20, 20, 0, -20, 20, BTN_REROLL, 20, 40, (press) -> {
-                this.nameEdit.setValue(NamingUnconvention.RANDOM_NAME_GENERATOR.generateRandomName());
-            }), helperGrid.newCellSettings().paddingHorizontal(4));
-            worldNameGrid.cellInhabitants.set(1, new GridLayout.CellInhabitant())
-            worldNameGrid.children.set(1, helperGrid.getGrid());
-        }
-        /*this.rerollButton = new ImageButton(this.nameEdit.getX() + this.nameEdit.getWidth(), this.nameEdit.getY(), 20, 20, 0, -20, 20, BTN_REROLL, 20, 40, (press) -> {
+        this.rerollButton = new ImageButton(216 + this.nameEdit.getWidth() + 4, 66, 20, 20, 0, -20, 20, BTN_REROLL, 20, 40, (press) -> {
             this.nameEdit.setValue(NamingUnconvention.RANDOM_NAME_GENERATOR.generateRandomName());
         });
-        this$0.addRenderableWidget(rerollButton);*/
+    }
 
-        /*this.rerollButton = (new ImageButton(this.nameEdit.getX() + 210, 60, 20, 20, 0, -20, 20, BTN_REROLL, 20, 40, (press) -> {
-            this.nameEdit.setValue(NamingUnconvention.RANDOM_NAME_GENERATOR.generateRandomName());
-        }));*/
+    @Override
+    public void visitChildren(Consumer<AbstractWidget> pConsumer) {
+        super.visitChildren(pConsumer);
+        pConsumer.accept(rerollButton);
     }
 }
